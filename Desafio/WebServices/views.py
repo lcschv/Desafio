448cb09758b2f1e98ee4fe3835b165e18c0e4dae
Lucas <lcs.chv@gmail.com>
2016-01-18 03:53:32 -0200

    Finished access to database.

--- Desafio/WebServices/views.py
+++ Desafio/WebServices/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.db.models import Q
 from WebServices.models import Company, Servers
@@ -19,26 +19,31 @@ def search(request):
         q = request.GET['q']
         if choice == 'Company':
 	        companies = Company.objects.filter(Q(name__icontains=q)|Q(address__icontains=q))
+	        servers = Servers.objects.order_by('preco')
 	        return render(request, 'search_results.html',
-	            {'companies': companies, 'query': q})
+	            {'companies': companies, 'query': q,
+	            'servers':servers})
     	
     	if choice == 'CPU':
-    		servers= Servers.objects.filter(Q(cpu__icontains=q))
+    		servers= Servers.objects.filter(Q(cpu__icontains=q)).order_by('preco')
     		return render(request,'search_results.html',
     			{'servers':servers,'query':q})
     	
     	if choice == 'HD':
-    		servers= Servers.objects.filter(Q(disco__icontains=q))
+    		servers= Servers.objects.filter(Q(disco__icontains=q)).order_by('preco')
     		return render(request,'search_results.html',
     			{'servers':servers,'query':q})
     	
     	if choice == 'Memory':
-    		servers= Servers.objects.filter(Q(memoria__icontains=q))
+    		servers= Servers.objects.filter(Q(memoria__icontains=q)).order_by('preco')
+    		servers.objects.order_by('preco')
     		return render(request,'search_results.html',
     			{'servers':servers,'query':q})
     else:
     	companies = Company.objects.all()
+    	servers = Servers.objects.all().order_by('preco')
         return render(request, 'search_results.html',
-            {'companies': companies})
+            {'companies': companies,
+            'servers':servers})
 
 
